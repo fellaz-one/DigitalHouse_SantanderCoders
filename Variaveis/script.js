4fd3f3a8bf0c8bc29dd41387b31d98f8a220c82f
Gabi Pires <gabrielatrindadepires@gmail.com>
2020-07-07 18:49:07 -0300

    Estudo de Variaveis

---
+++ Variaveis/script.js
@@ -0,0 +1,13 @@
+// ===== CRIANDO VARIÁVEIS ====== \\
+
+// Declare duas variáveis chamadas  idade  e  peso,  e atribua um valor numérico a ambas.
+
+var idade = 28;
+var peso = 60;
+
+// Declare 4 variáveis: nome, sobrenome, numeroDaSorte, idade e atribua seus respectivos valores nas variáveis.
+
+var nome ="Gabriela";
+var sobrenome="Pires";
+var numeroDaSorte=17;
+var idade=28;
